45e1b9007ac5fccc78f1c3829b01557ac49db81f
Graeme Paterson <ghpaterson@gmail.com>
2023-06-15 14:14:03 +0100

    NavBar component

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from "./hero";
 import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 export {
     Hero,
     CustomButton,
+    NavBar,
+    Footer,
 }
