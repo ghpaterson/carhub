a6db4d1fdfce4a376b3f5647fee709256ed3c36e
Graeme Paterson <ghpaterson@gmail.com>
2023-06-16 13:58:34 +0100

    started search functionality

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,17 @@ import Hero from "./hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
     Hero,
     CustomButton,
     NavBar,
     Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+
 }
